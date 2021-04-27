4d17ed10ebb2f7dfc3fa60625639d040eef5277a
zqm64 <zqm64@outlook.com>
2021-04-27 09:15:17 +0800

    power: Add mi8937 FP processes to use Oneplus 3 fingerprintd thaw hack

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -203,8 +203,11 @@ void thaw_fingerprintd(void)
 	read_lock(&tasklist_lock);
 	for_each_process(p) {
 		if ((!memcmp(p->comm, "android.hardware.biometrics.fingerprint@2.1-service", 13)) ||
+			(!memcmp(p->comm,"android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8937", 13)) ||
 			(!memcmp(p->comm,"android.hardware.biometrics.fingerprint@2.1-service.custom", 13)) ||
 			(!memcmp(p->comm,"android.hardware.biometrics.fingerprint@2.1-service.land", 13)) ||
+			(!memcmp(p->comm,"land_gx_fpd", 13)) ||
+			(!memcmp(p->comm,"santoni_gx_fpd", 13)) ||
 			(!memcmp(p->comm,"gx_fpd", 13))) {
 			__thaw_task(p);
 			break;
